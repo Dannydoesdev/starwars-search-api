996189feeb6bd171eb76d07656db8e1a41a66e56
Dannydoesdev <daniel.t.mcgee@gmail.com>
2022-12-04 21:28:35 +1100

    character page loading (without tooltip), cleaned up code

--- swapi-test-next/hooks/useApiSearch.tsx
+++ swapi-test-next/hooks/useApiSearch.tsx
@@ -1,9 +1,17 @@
 import useSWR, { Key } from "swr"
 
+interface ReturnObject {
+  name: string,
+  data: object
+}
+interface ReturnArray {
+  data?: Array<ReturnObject>
+}
+
 const fetcher = (url: URL) => fetch(url).then(res => res.json())
 
-function useApiSearch(endpoint:string, id:string) {
-  
+export function useApiSearch(endpoint: string, id: string) {
+
   const { data, error } = useSWR(`https://www.swapi.tech/api/${endpoint}/${id}`, fetcher)
 
   return {
@@ -12,84 +20,28 @@ function useApiSearch(endpoint:string, id:string) {
     isLoading: !error && !data,
     isError: error
   }
-
 }
 
-
 function useApiArray(endpointArray: string[]) {
-  
+
   if (!endpointArray) {
     console.log('no URL received in hook')
   }
-  console.log(endpointArray)  
 
-  interface ReturnObject {
-    name: string,
-    data: object
-  }
-  interface ReturnArray {
-    data?: Array<ReturnObject>
-  }
-  // const returnArr: ReturnObject[] = [];
   const returnArr: any = [];
 
-
-  // let returnObj: returnObject = [
-   
-  //   // [key: string]: Object[]
-  // ]
-  
   endpointArray.map((endpoint) => {
-    console.log(endpoint)
-    const { data } = useSWR(`${endpoint}`, fetcher)
-    if (data) { 
-      console.log(data)
+    const { data } = useSWR(`${endpoint}`, fetcher, { dedupingInterval: 2000 })
+    if (data) {
       let resultObj = {
         name: data.result.properties.name,
         data: data.result.properties
       }
-      
-    returnArr.push(resultObj)
-
-    // let resultName: string = data.result.properties.name;
-    // let resultData: object = data.result.properties
-      
-
-      // returnObj[resultName] = resultData;
-      // console.log(returnObj)
-  }
-
+      returnArr.push(resultObj)
+    }
   })
 
-  // return { data: returnObj };
-  console.log(returnArr)
   return returnArr;
-  // const { data, error } = useSWR(`https://www.swapi.tech/api/${endpoint}/${id}`, fetcher)
-
-  // return {
-  //   result: data.result,
-  //   properties: data.result.properties,
-  //   isLoading: !error && !data,
-  //   isError: error
-  // }
-
 }
-
-// Using serverside/static props with fetcher
-
-// https://swr.vercel.app/examples/ssr
-// const fetcher = (url) => fetch(url).then((res) => res.json());
-// const API = "https://api.github.com/repos/vercel/swr";
-
-// export async function getServerSideProps() {
-//   const repoInfo = await fetcher(API);
-//   return {
-//     props: {
-//       fallback: {
-//         [API]: repoInfo
-//       }
-//     }
-//   };
-// }
 
 export default useApiArray
--- swapi-test-next/pages/films/[id].tsx
+++ swapi-test-next/pages/films/[id].tsx
@@ -2,10 +2,9 @@ import { useRouter } from 'next/router';
 import { GetStaticPaths, GetStaticProps, InferGetStaticPropsType } from 'next';
 import { IndividualFilm, FilmResult } from '../../interfaces';
 import Link from 'next/link';
-import useSWR from 'swr'
-import axios from 'axios'
 import { Key, useEffect, useState } from 'react'
 import useArraySearch from '../../hooks/useApiSearch';
+import Character from '../../components/MoviePage/Character';
 
 export const getStaticPaths: GetStaticPaths = async () => {
   const res = await fetch(`https://www.swapi.tech/api/films/`)
@@ -26,42 +25,30 @@ export const getStaticPaths: GetStaticPaths = async () => {
 export const getStaticProps: GetStaticProps = async ({ params }: any) => {
   const res = await fetch(`https://www.swapi.tech/api/films/${params.id}`);
   const { result } = await res.json();
-  // const { data } = useArraySearch(film.characters)
-  // const { data } = useArraySearch(result.properties.characters)
-
 
   return {
     props: {
       film: result.properties
     },
-    revalidate: 100
+    revalidate: 1000
   };
 }
 
 function Film({ film }: InferGetStaticPropsType<typeof getStaticProps>) {
   const router = useRouter();
 
-  // const [characters, setCharacters] = useState<any>()
-
-  // useEffect(() => {
-  //   const { data } = useArraySearch(film.characters)
-  //   setCharacters(data)
-
-  // }, [])
-  
-
-  const data  = useArraySearch(film.characters)
-  console.log(data)
+  const data = useArraySearch(film.characters)
+  // console.log(data)
 
   if (!data) {
     return (
       <>
-      <Link href='/'>
-        Home
-      </Link>
+        <Link href='/'>
+          Home
+        </Link>
         <h1>{film.title}</h1>
         <p>Loading movies...</p>
-    </>
+      </>
     )
   }
 
@@ -72,12 +59,18 @@ function Film({ film }: InferGetStaticPropsType<typeof getStaticProps>) {
       </Link>
       <h1>{film.title}</h1>
       <h2>Characters:</h2>
-      {/* <p>{data[0][0].name}</p> */}
-      {/* {data.map((char:any) => {
-       return <p> {char} </p>
-      })} */}
-      {data.map((char: any) => {
-        return <p>{char.name}</p>
+      {data.map((char: any, index: Key | null | undefined) => {
+        return (
+          <Character
+            key={index}
+            name={char.name}
+            skinColor={char.data.skin_color}
+            birthYear={char.data.birth_year}
+            eyeColour={char.data.eye_color}
+            hairColour={char.data.hair_color}
+            gender={char.data.gender}
+          />
+        )
       })}
     </>
   )
