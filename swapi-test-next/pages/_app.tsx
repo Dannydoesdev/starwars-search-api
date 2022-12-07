b8e97253031a0b7c491ef46931f87561f1c598ec
Dannydoesdev <daniel.t.mcgee@gmail.com>
2022-12-07 23:35:53 +1100

    Further cleanup

--- swapi-test-next/pages/_app.tsx
+++ swapi-test-next/pages/_app.tsx
@@ -1,6 +1,6 @@
 import '../styles/globals.scss'
 import type { AppProps } from 'next/app'
-import NavBar from '../components/Navbar/Navbar'
+import NavBar from '../components/Navbar/NavBar'
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
